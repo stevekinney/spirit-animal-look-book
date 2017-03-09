620d96b97a0ea11daaab512005186c01d927e619
Steve Kinney <hello@stevekinney.net>
2017-03-09 15:43:10 -0600

    Remove Firebase keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyBs-HL-vvEhMORbdeD2JsKloNw1-bqPo9k',
-  authDomain: 'spirit-animal-look-book.firebaseapp.com',
-  databaseURL: 'https://spirit-animal-look-book.firebaseio.com',
-  storageBucket: 'spirit-animal-look-book.appspot.com',
-  messagingSenderId: '364594286380'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  messagingSenderId: ''
 };
 
 firebase.initializeApp(config);
